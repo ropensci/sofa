1caaf0fb594fd7e58db9dfc2dfcf4e82699d99d0
schamberlain <myrmecocystus@gmail.com>
2013-06-21 14:31:55 -0700

    added cloudant iriscouch ability to ping fxn

--- R/sofa_ping.r
+++ R/sofa_ping.r
@@ -2,10 +2,27 @@
 #' 
 #' @param endpoint the endpoint, defaults to localhost (http://127.0.0.1)
 #' @param port port to connect to, defaults to 5984
+#' @param username Your cloudant or iriscouch username
+#' @param pwd Your cloudant or iriscouch password
 #' @examples
 #' sofa_ping()
 #' @export
-sofa_ping <- function(endpoint="http://127.0.0.1", port=5984)
+sofa_ping <- function(endpoint="localhost", port=5984, username=NULL, pwd=NULL)
 {
-  fromJSON(content(GET(paste(endpoint, port, sep=":"))))
+  endpoint <- match.arg(endpoint,choices=c("localhost","cloudant","iriscouch"))
+  
+  if(endpoint=="localhost"){
+    call_ <- sprintf("http://127.0.0.1:%s", port)
+    fromJSON(content(GET(call_)))
+  } else
+    if(endpoint=="cloudant"){
+      auth <- get_pwd(username,pwd,"cloudant")
+      call_ <- sprintf('https://%s:%s@%s.cloudant.com', auth[[1]], auth[[2]], auth[[1]])
+      fromJSON(content(GET(call_)))
+    } else
+    {
+      auth <- get_pwd(username,pwd,"iriscouch")
+      call_ <- sprintf('https://%s.iriscouch.com', auth[[1]])
+      fromJSON(content(GET(call_)))
+    }  
 }
