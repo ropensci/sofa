75d6f1078f037ce70d4e6cf5c3be9fda695ac57c
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-13 13:02:47 -0800

    added pkg level man file

---
+++ R/sofa-package.r
@@ -0,0 +1,9 @@
+#' R client for CouchDB.
+#'
+#' @name sofa-package
+#' @aliases sofa
+#' @docType package
+#' @title R client for CouchDB
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @keywords package
+NULL
