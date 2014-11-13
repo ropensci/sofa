10e70e6ffaefbb7dd8fe8ffc4f94efc51b5b6faf
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-13 12:42:57 -0800

    imprvoed startup message, removed elasticsearch reference

--- R/zzz.r
+++ R/zzz.r
@@ -3,11 +3,9 @@ SofaAuthCache <- new.env(hash=TRUE)
 
 .onAttach <- function(...) {
   packageStartupMessage("
-    \nStart CouchDB on your command line by typing 'couchdb'
-    \nThen start Elasticsearch if using by opening a new terminal tab/window, navigating to where it was installed and starting
-    \nOn my Mac this is: cd /usr/local/elasticsearch then bin/elasticsearch -f
-    \nNew to sofa? Tutorial at https://github.com/sckott/sofa.
-    \nUse suppressPackageStartupMessages() to suppress these startup messages in the future\n
+    Start CouchDB on your command line by typing 'couchdb'
+    New to sofa? Tutorial at https://github.com/sckott/sofa
+    Use suppressPackageStartupMessages() to suppress these startup messages in the future\n
   ")
 }
 
