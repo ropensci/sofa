9547a788e18d55dda63e99e29cbc4bf499e8f0c9
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-14 08:17:34 -0800

    added simplify param to revisions fxn to get back as just revs or revs and avail

--- R/revisions.r
+++ R/revisions.r
@@ -5,12 +5,16 @@
 #' @param cushion A cushion name
 #' @param dbname Database name
 #' @param docid Document ID
+#' @param simplify (logical) Simplify to character vector of revision ids. If FALSE, gives back
+#' availabilit info too.
 #' @examples \donttest{
 #' revisions(dbname="sofadb", docid="a_beer")
+#' revisions(dbname="sofadb", docid="a_beer", simplify=FALSE)
 #' revisions(dbname="sofadb", docid="a_beer", as='json')
+#' revisions(dbname="sofadb", docid="a_beer", simplify=FALSE, as='json')
 #' }
 
-revisions <- function(cushion="localhost", dbname, docid, as='list', ...)
+revisions <- function(cushion="localhost", dbname, docid, simplify=TRUE, as='list', ...)
 {
   cushion <- get_cushion(cushion)
   if(cushion$type=="localhost"){
@@ -18,6 +22,6 @@ revisions <- function(cushion="localhost", dbname, docid, as='list', ...)
   } else if(cushion$type %in% c("cloudant",'iriscouch')){
     tmp <- sofa_GET(file.path(remote_url(cushion, dbname), docid), list(revs_info='true'), 'list', content_type_json(), ...)
   }
-  revs <- vapply(tmp$`_revs_info`, "[[", "", "rev")
+  revs <- if(simplify) vapply(tmp$`_revs_info`, "[[", "", "rev") else tmp$`_revs_info`
   if(as=='json') jsonlite::toJSON(revs) else revs
 }
