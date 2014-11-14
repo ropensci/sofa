9cd729777f3f13abd168f1f1f18b1326b68053c3
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-14 09:11:01 -0800

    added documents man file for overview of docs fxns

---
+++ R/documents.r
@@ -0,0 +1,17 @@
+#' Work with documents in your CouchDB's.
+#'
+#' @name documents
+#' @details If you are writing a complicated javascript function, better to do
+#' that in the Futon CouchDB interface or otherwise.
+#'
+#' There are the following functions for working with documents:
+#' \itemize{
+#'  \item \code{\link{doc_create}} - Create a document, with or without an ID
+#'  \item \code{\link{doc_update}} - Update a document
+#'  \item \code{\link{doc_get}} - Get a document
+#'  \item \code{\link{doc_delete}} - Delete a document
+#'  \item \code{\link{doc_head}} - Get headers for a document
+#'  \item \code{\link{alldocs}} - Get all documents
+#'  \item \code{\link{revisions}} - Get revisions for a document
+#' }
+NULL
