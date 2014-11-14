0ee7297c988c05967632cba3452629ea06235d9b
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-14 08:21:40 -0800

    renamed sf_head to headdoc

--- R/headdoc.r
+++ R/headdoc.r
@@ -5,12 +5,13 @@
 #' @param dbname Database name. (charcter)
 #' @param docid Document ID (character)
 #' @examples \donttest{
-#' sf_head(dbname="sofadb", docid="a_beer")
-#' sf_head(dbname="sofadb", docid="a_beer", as='json')
-#' sf_head("cloudant", dbname="animaldb", docid="badger")
-#' sf_head("iriscouch", dbname="helloworld", docid="ggg")
+#' headdoc(dbname="sofadb", docid="a_beer")
+#' headdoc(dbname="sofadb", docid="a_beer", as='json')
+#' headdoc("cloudant", dbname="animaldb", docid="badger")
+#' headdoc("iriscouch", dbname="helloworld", docid="ggg")
 #' }
-sf_head <- function(cushion="localhost", dbname, docid, as='list', ...)
+
+headdoc <- function(cushion="localhost", dbname, docid, as='list', ...)
 {
   cushion <- get_cushion(cushion)
   if(cushion$type=="localhost"){
@@ -20,5 +21,5 @@ sf_head <- function(cushion="localhost", dbname, docid, as='list', ...)
   }
   out <- HEAD(call_, ...)
   stop_for_status(out)
-  if(as=='lis') out$all_headers else jsonlite::toJSON(out$all_header)
+  if(as=='list') out$all_headers else jsonlite::toJSON(out$all_header)
 }
