efec2edfd9e2e4fe48eb699b8d1c860b5a610749
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-19 15:17:32 -0800

    fixed auth to pull from a hidden file closes #22

--- R/auth.r
+++ R/auth.r
@@ -10,6 +10,7 @@
 #' @param type One of localhost, cloudant, or iriscouch. This is what's used to determine
 #' how to structure the URL to make the request.
 #' @param port Port. Only applies when type is localhost. Default: 5984
+#' @param authfile Path to file with cushions (authentication details)
 #' @details Setup for authentication:
 #' For localhost you don't need to authenticate, but of course you may have set
 #' up a username and password in which case use 'localhost'.
@@ -18,21 +19,24 @@
 #'
 #' You can use other named username/password sets too.
 #'
-#' You can permanently store your auth details in your .Rprofile by putting in
-#' entries like this:
+#' You can permanently store your auth details in a hidden file ~/.sofa-auth by putting in
+#' entries like this, each on a new line:
 #'
-#' options(cloudant = list(user='name', pwd='pwd'))
+#' list(name="iriscouch", user="jane_doe", pwd="my_password", type="iriscouch")
 #'
-#' Though if you don't want to store them permanently, you can use the cushion()
-#' function instead. See examples below on how to do this. Though using cusion()
-#' only stores them for the current session.
+#' Though if you don't want to store them permanently, you can use the \code{cushion}
+#' function instead. See examples below on how to do this. Using \code{cushion} only
+#' stores them for the current session.
 #'
 #' \code{cushions()} Looks first in the local environment SofaAuthCache, and if finds nothing,
-#' looks in your \code{.Rprofile} file.
+#' looks in your \code{~/.sofa-auth} file, or wherver you set \code{authfilie} parameter to.
+#'
+#' Beware that you should use unique names for each cushion, that is, in the \code{name} parameter.
 #' @examples \donttest{
 #' cushion('cloudant', user='name', pwd='pwd', type="cloudant")
 #' cushion('iriscouch', user='name', pwd='pwd', type="iriscouch")
 #' cushion('julies_iris', user='name', pwd='pwd', type="iriscouch")
+#' cushion('adfafafafadf', user='name', pwd='pwd', type="localhost", port=2300)
 #' cushions()
 #' }
 
@@ -44,20 +48,44 @@ cushion <- function(name, user=NULL, pwd=NULL, type, port=5984){
 
 #' @export
 #' @rdname authentication
-cushions <- function()
+cushions <- function(authfile = "~/.sofa-auth")
 {
-  if(length(ls(SofaAuthCache)) == 0){
-    vals <- names(.Options)
-    temp <- .Options[grep('sofa', vals)]
-  } else { temp <- mget(ls(SofaAuthCache), envir=SofaAuthCache) }
-  if(length(temp) == 0)
+  # make file if doesn't exist
+  if(!file.exists(authfile)) cat("", file = authfile)
+  lns <- readLines(authfile)
+  profs <- lapply(lns, makelist)
+  temp <- mget(ls(SofaAuthCache), envir=SofaAuthCache)
+  nms <- unlist(c(sapply(profs, function(x) names(x)), sapply(list(temp), function(x) names(x))))
+  if(length(nms) != 0) dups <- paste0(sapply(nms[ duplicated(nms) ], function(x) sprintf('"%s"', x)), collapse = " ")
+  if(any(duplicated(nms))) stop(sprintf("%s found in both cached auth file and in the current session", dups))
+  comb <- do.call(c, c(profs, list(temp)))
+  if(length(comb) == 0)
     stop("No auth details found")
   else
-    lapply(temp, function(x) structure(x, class="sofa_profiles"))
+    lapply(comb, function(x) structure(x, class="sofa_profile"))
 }
 
+makelist <- function(x){
+  tmp <- eval(parse(text=x))
+  aname <- tmp$name
+  tmp <- tmp[ !names(tmp) %in% "name" ]
+  structure(list(tmp), .Names=aname)
+}
+
+# cushions <- function()
+# {
+#   if(length(ls(SofaAuthCache)) == 0){
+#     vals <- names(.Options)
+#     temp <- .Options[grep('sofa', vals)]
+#   } else { temp <- mget(ls(SofaAuthCache), envir=SofaAuthCache) }
+#   if(length(temp) == 0)
+#     stop("No auth details found")
+#   else
+#     lapply(temp, function(x) structure(x, class="sofa_profile"))
+# }
+
 #' @export
-print.sofa_profiles <- function(x, ...){
+print.sofa_profile <- function(x, ...){
   cat("<sofa profile> ", sep = "\n")
   cat(paste0("   user : ", x$user), sep = "\n")
   cat(paste0("   pwd  : ", x$pwd), sep = "\n")
