cf95f07af58a2e3b79c30d58c18d8954d03d9cea
Scott Chamberlain <myrmecocystus@gmail.com>
2016-07-25 17:50:09 -0700

    fix test

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,10 +1,11 @@
 library("testthat")
+library("sofa")
 
 invisible(x <- sofa::Cushion$new())
 
 db <- "testing123"
-if ("error" %in% names(db_info(x, db))) {
-  db_create(x, dbname = db)
+if ("error" %in% names(sofa::db_info(x, db))) {
+  sofa::db_create(x, dbname = db)
 }
 invisible(sofa::bulk_create(x, dbname = db, doc = iris))
 
